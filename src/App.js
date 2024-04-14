c56c2598d2be2bfcbeb399e2cf869e422d21bd87
LewisBamf <bamfordlewis9@gmail.com>
2024-04-14 16:22:43 +0100

    added own react content (hello world)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import react from 'react';
+import Message from './message'; // Corrected import statement
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+
+  <div className='App'>
+   <div className='page-grid'>
+     <Message />
     </div>
-  );
+  </div>
+  )
 }
 
 export default App;
---
+++ src/message.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+
+
+function Message(){
+    return(
+        <div className='page-item'>
+        <h1 className='helloTXT'>hello world!</h1>
+        </div>
+    )
+}
+
+export default Message;
